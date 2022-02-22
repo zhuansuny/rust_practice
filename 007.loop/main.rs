5ff8193d731de749939da323a719b87c78591294
zhuansuny <18322596935@163.com>
2022-02-22 22:29:58 +0800

    loop

---
+++ 007.loop/main.rs
@@ -0,0 +1,28 @@
+fn main(){
+
+    loop{
+        println!("again!");
+        break;
+    }
+
+    let mut number = 5;
+     while number != 0{
+        println!("{}!", number);
+        number -= 1;
+     }
+
+     let arr = [10, 20, 30, 40, 50];
+
+     for a in arr {
+         println!("the value is: {}", a);
+         
+     }
+
+     // (1..5)range从1到5，不包含5（左闭右开）
+     // (1..=5)range从1到5，包含5（左闭右闭）
+     // rev 是反转
+     
+     for num in (1..5).rev(){
+            println!("{}!", num);
+     }
+}
