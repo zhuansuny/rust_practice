d875a66fc27772b987842c589d1fb6e83bf1b44c
zhuansuny <18322596935@163.com>
2022-03-02 17:48:14 +0800

    fix(mod) remove target

--- 016.mod/target/debug/deps/project-d1ff803f57b9974c.d
+++
@@ -1,7 +0,0 @@
-/Users/izhaohu/rustProject/rust_practice/016.mod/target/debug/deps/project-d1ff803f57b9974c.rmeta: src/main.rs src/back_of_house.rs src/front_of_house.rs
-
-/Users/izhaohu/rustProject/rust_practice/016.mod/target/debug/deps/project-d1ff803f57b9974c.d: src/main.rs src/back_of_house.rs src/front_of_house.rs
-
-src/main.rs:
-src/back_of_house.rs:
-src/front_of_house.rs:
