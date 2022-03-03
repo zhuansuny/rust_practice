cc910c21b7bf35f914668545b7acea02432f93f6
zhuansuny <18322596935@163.com>
2022-03-03 15:57:54 +0800

    feat(test)

---
+++ 024.test/target/debug/deps/adder-0609eda80b66ac9a.d
@@ -0,0 +1,5 @@
+/Users/izhaohu/rustProject/rust_practice/adder/target/debug/deps/adder-0609eda80b66ac9a.rmeta: src/lib.rs
+
+/Users/izhaohu/rustProject/rust_practice/adder/target/debug/deps/adder-0609eda80b66ac9a.d: src/lib.rs
+
+src/lib.rs:
---
+++ 024.test/target/debug/deps/adder-f7fe3d092a386eeb.d
@@ -0,0 +1,7 @@
+/Users/izhaohu/rustProject/rust_practice/adder/target/debug/deps/adder-f7fe3d092a386eeb.rmeta: src/lib.rs
+
+/Users/izhaohu/rustProject/rust_practice/adder/target/debug/deps/libadder-f7fe3d092a386eeb.rlib: src/lib.rs
+
+/Users/izhaohu/rustProject/rust_practice/adder/target/debug/deps/adder-f7fe3d092a386eeb.d: src/lib.rs
+
+src/lib.rs:
