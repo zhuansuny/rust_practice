f86107a279c3cbc16ea4287d9748d0e893805d65
zhuansuny <18322596935@163.com>
2022-03-10 17:57:29 +0800

    delete target

--- 031.drop/target/debug/deps/drop-226e11dfb7f69c6c.d
+++
@@ -1,5 +0,0 @@
-/Users/izhaohu/rustProject/rust_practice/031.drop/target/debug/deps/drop-226e11dfb7f69c6c: src/main.rs
-
-/Users/izhaohu/rustProject/rust_practice/031.drop/target/debug/deps/drop-226e11dfb7f69c6c.d: src/main.rs
-
-src/main.rs:
--- 031.drop/target/debug/drop.d
+++
@@ -1,1 +0,0 @@
-/Users/izhaohu/rustProject/rust_practice/031.drop/target/debug/drop: /Users/izhaohu/rustProject/rust_practice/031.drop/src/main.rs
